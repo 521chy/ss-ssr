892e219575b61d8bc5626fe31d6977076b0c986e
521chy <58168076+521chy@users.noreply.github.com>
2020-05-05 08:30:45 +0800

    Update shadowsocks-libev.sh
    https://tls.mbed.org/download/mbedtls-2.16.6-gpl.tgz

--- shadowsocks-libev.sh
+++ shadowsocks-libev.sh
@@ -20,7 +20,7 @@ libsodium_url="https://github.com/jedisct1/libsodium/releases/download/1.0.18-RE
 #mbedtls_file="mbedtls-2.11.0"
 #mbedtls_url="https://tls.mbed.org/download/mbedtls-2.11.0-gpl.tgz"
 mbedtls_file="mbedtls-2.16.6"
-mbedtls_url="https://tls.mbed.org/download/start/mbedtls-2.16.6-gpl.tgz"
+mbedtls_url="https://tls.mbed.org/download/mbedtls-2.16.6-gpl.tgz"
 
 # Stream Ciphers
 ciphers=(
